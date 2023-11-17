03323af15507bc471dcd27a5e3d779b3529a685a
Pat Needham <patrick.h.needham@gmail.com>
2023-11-17 01:53:43 -0500

    chakra ui added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ChakraProvider resetCSS theme={theme}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Failed to find the root element');
+}
