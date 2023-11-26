18f582af36625159c426e58246cf05a97aae0a10
Pat Needham <patrick.h.needham@gmail.com>
2023-11-26 18:28:59 -0500

    bun to npm since remix runs better that way and don't really need fast builds right now; skip lib check for chakra ui; client running from /app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [remix(), react(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths()],
 });
