20211a82ce247ebd5cc6ed29bb8cd2594ce7b097
Pat Needham <patrick.h.needham@gmail.com>
2023-11-26 19:18:29 -0500

    src removed

--- src/lib/styles/theme/config.ts
+++
@@ -1,6 +0,0 @@
-import type { ThemeConfig } from '@chakra-ui/react';
-
-export const config: ThemeConfig = {
-  // https://chakra-ui.com/getting-started/migration#3-color-mode-update
-  disableTransitionOnChange: false,
-};
