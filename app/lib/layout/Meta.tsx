5022ad36e724333382d28f14046e8f9acd2d2b06
Pat Needham <patrick.h.needham@gmail.com>
2023-12-04 15:41:46 -0500

    new favicon

--- app/lib/layout/Meta.tsx
+++ app/lib/layout/Meta.tsx
@@ -16,7 +16,6 @@ const Meta = () => {
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="theme-color" content="#FFFFFF" />
 
-      <link rel="shortcut icon" href="/assets/favicon.svg" />
       <link rel="manifest" href="/manifest.json" />
     </Helmet>
   );
