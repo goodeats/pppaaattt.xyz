16cd8e50989f65453f3ad86bf2a059f10981a2c2
Pat Needham <patrick.h.needham@gmail.com>
2023-12-04 23:27:45 -0500

    navs to index

--- app/routes/dashboard+/builder+/_builder.tsx
+++ app/routes/dashboard+/builder+/_builder.tsx
@@ -1,15 +1,4 @@
-import {
-  Box,
-  BreadcrumbLink,
-  BreadcrumbLinkType,
-  Button,
-  ButtonGroup,
-  Flex,
-  Stack,
-  StackDivider,
-  useColorModeValue,
-} from '~/components';
-import { ColumnHeading } from '~/lib/layout/columns/_shared.tsx';
+import { Flex, Stack, StackDivider } from '~/components';
 import { NavLink, Outlet } from '@remix-run/react';
 
 export const handle = {
@@ -17,43 +6,10 @@ export const handle = {
 };
 
 export default function BuilderPage() {
-  const navs = [
-    { id: 'layers', title: 'Layers' },
-    { id: 'design-attributes', title: 'Design Attributes' },
-  ];
-
-  const NavGroup = () => {
-    const bgActive = useColorModeValue('gray.100', 'gray.800');
-    const bgHover = useColorModeValue('gray.300', 'gray.600');
-
-    return (
-      <Box as="nav" aria-label="Builder navigation" marginBottom={3}>
-        <ButtonGroup variant="outline">
-          {navs.map((nav, i) => (
-            <NavLink key={i} to={nav.id}>
-              {({ isActive, isPending }) => (
-                <Button
-                  bg={isActive ? bgActive : undefined}
-                  _hover={{ bg: bgHover }}
-                  transition="background-color 0.2s ease-in-out"
-                  disabled={isPending}
-                >
-                  {nav.title}
-                </Button>
-              )}
-            </NavLink>
-          ))}
-        </ButtonGroup>
-      </Box>
-    );
-  };
-
   return (
     <>
-      <ColumnHeading>Builder</ColumnHeading>
       <Flex flex={1} flexDirection="column">
         <Stack divider={<StackDivider borderColor="gray.300" />}>
-          <NavGroup />
           <Outlet />
         </Stack>
       </Flex>
--- app/routes/dashboard+/builder+/index.tsx
+++ app/routes/dashboard+/builder+/index.tsx
@@ -1,9 +1,15 @@
-import { Stack, Text } from '~/components';
+import { CustomButtonNavGroup, Stack, Text } from '~/components';
 
 export default function BuilderIndexPage() {
+  const navs = [
+    { to: 'layers', title: 'Layers' },
+    { to: 'design-attributes', title: 'Design Attributes' },
+  ];
+
   return (
     <Stack width="full" paddingX={8} paddingY={5} textAlign="left">
       <Text fontSize="sm">Here is where you will customize the builds</Text>
+      <CustomButtonNavGroup navs={navs} />
     </Stack>
   );
 }
