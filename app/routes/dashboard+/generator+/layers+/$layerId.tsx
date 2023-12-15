7927c5b5173f46c27611cc4075de4938e10b5a8b
Pat Needham <patrick.h.needham@gmail.com>
2023-12-15 17:54:23 -0500

    gen layer show

---
+++ app/routes/dashboard+/generator+/layers+/$layerId.tsx
@@ -0,0 +1,71 @@
+import { ContentContainer, LayerCard } from '~/components';
+import { DataFunctionArgs, json, redirect } from '@remix-run/node';
+import { NavLink, Outlet, useLoaderData } from '@remix-run/react';
+import { prisma } from '~/utils/db.server';
+
+export const handle = {
+  breadcrumb: (match) => {
+    const { data, params } = match;
+    const layerId = params.layerId;
+    const title = data.layer?.title ?? 'Layer';
+    return (
+      <NavLink to={`/dashboard/builder/layers/${layerId}`}>{title}</NavLink>
+    );
+  },
+};
+
+export async function loader({ params }: DataFunctionArgs) {
+  const { layerId } = params;
+  if (!layerId) {
+    return redirect('/layers');
+  }
+
+  const layer = await prisma.layer.findUnique({
+    where: {
+      id: layerId,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      createdAt: true,
+      updatedAt: true,
+      designAttributes: {
+        select: {
+          id: true,
+          title: true,
+          attributeType: true,
+        },
+      },
+      _count: {
+        select: { designAttributes: true },
+      },
+    },
+  });
+
+  if (!layer) {
+    // TODO: redirect to 404 page
+    // create toast notification
+    return redirect('/dashboard/generator/layers?notFound=true');
+  }
+
+  return json({ layer });
+}
+
+export default function LayerDetailsPage() {
+  const data = useLoaderData<typeof loader>();
+  const { layer } = data;
+
+  return (
+    <ContentContainer>
+      <LayerCard
+        layer={{
+          ...layer,
+          createdAt: new Date(layer.createdAt),
+          updatedAt: new Date(layer.updatedAt),
+        }}
+      />
+      <Outlet />
+    </ContentContainer>
+  );
+}
