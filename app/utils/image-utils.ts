3c10b459a4b0754e581b47d88cafa5eb996cd5fd
Pat Needham <patrick.h.needham@gmail.com>
2023-12-28 11:03:28 -0500

    load image and return it

---
+++ app/utils/image-utils.ts
@@ -0,0 +1,26 @@
+type ImageLoadProps = {
+  imageSrc: string;
+};
+
+export const ImageLoad = async ({
+  imageSrc,
+}: ImageLoadProps): Promise<HTMLImageElement> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.onload = () => {
+      resolve(img);
+    };
+
+    img.onerror = () => {
+      reject(new Error(`Failed to load image from source: ${imageSrc}`));
+    };
+
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image
+    // add this so we can use the image in a canvas
+    // and not get a tainted canvas error
+    img.crossOrigin = 'anonymous';
+
+    img.src = imageSrc;
+  });
+};
