c40e60eb8e66afb54290db15853c3d9930850095
Pat Needham <patrick.h.needham@gmail.com>
2024-01-08 13:05:25 -0500

    images grid

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,8 @@
 import { DataFunctionArgs, json, redirect } from '@remix-run/node';
 import Column from '~/lib/layout/Column.tsx';
-import { ColumnHeading } from '~/lib/layout/columns/_shared.tsx';
 import { getUserId } from '~/modules/auth.server';
 import GuestLayout from '~/lib/layout/GuestLayout';
+import { ImagesGrid } from '~/components/models/landing/images-grid';
 
 export async function loader({ request }: DataFunctionArgs) {
   const userId = await getUserId(request);
@@ -16,7 +16,7 @@ export default function Index() {
   return (
     <GuestLayout>
       <Column>
-        <ColumnHeading>Welcome!</ColumnHeading>
+        <ImagesGrid />
       </Column>
     </GuestLayout>
   );
