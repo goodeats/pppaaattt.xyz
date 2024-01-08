f5be32872549bfdf811c8a6e56ead3e23dbd5719
Pat Needham <patrick.h.needham@gmail.com>
2024-01-08 01:12:42 -0500

    cleanup

--- app/utils/canvas/draw/templates.ts
+++ app/utils/canvas/draw/templates.ts
@@ -1,8 +1,3 @@
-import {
-  BuildAttributes,
-  BuildDimensions,
-  BuildPalette,
-} from '~/lib/utils/build-structure/build-attributes';
 import { CanvasDrawBackground } from './background';
 import { ContextBegin, ContextEnd } from './context';
 import { CanvasDrawTriangleAtCoords } from './triangle';
@@ -69,44 +64,21 @@ export const CanvasDrawTemplates = async ({
     ({ x, y, pixelColor, size, rotate, isBackground }, i) => {
       ContextBegin({ ctx });
 
-      // // Adjust size randomly within a range of a customizable percentage bigger or smaller
-      // const percentage = 1.5; // customize this value as needed
-      // const adjustment = Math.random() * 2 * percentage - percentage; // random number between -percentage and percentage
-      // // size = size * (1 + adjustment);
-      // size = isBackground ? 1 : size * (1 + adjustment);
-
-      // rotate = i % 2 === 0 ? rotate : 1;
-      // rotate = i % 2 === 0 ? 1.5 : 0.5;
-
       CanvasDrawTriangleAtCoords({
         ctx,
         triangle: { position: { x, y }, size, rotate },
       });
 
-      // const fillPixelColorAdjusted = adjustColorBrightness(pixelColor, -0.31);
-
       ctx.fillStyle = pixelColor;
-      // ctx.fillStyle = fillPixelColorAdjusted;
       if (isBackground) {
-        // const xmasColors = ['#f00', '#0f0'];
-        // const xmasColor = xmasColors[randomIndex(xmasColors)];
-        // ctx.fillStyle = xmascolor;
         // ctx.fill();
       } else if (i % 9 === 0) {
         ctx.fill();
       }
       // ctx.fill();
 
-      // ctx.lineWidth = 2;
       ctx.lineWidth = isBackground ? 6 : 3;
-
-      // const strokePixelColorAdjusted = adjustColorBrightness(
-      //   pixelColor,
-      //   -0.231
-      // );
-
       ctx.strokeStyle = pixelColor;
-      // ctx.strokeStyle = strokePixelColorAdjusted;
       ctx.stroke();
 
       ContextEnd({ ctx });
